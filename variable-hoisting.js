d832393b8c8d5a3b46287029629f715079a40660
Abraham Mejia <aimejia4@gmail.com>
2023-09-05 04:17:19 -0500

    Understood Variable Hoisting

---
+++ variable-hoisting.js
@@ -0,0 +1,34 @@
+// hello();
+
+// function hello(){
+//   console.log('Hello!');
+// }
+
+
+ let num;
+
+ num = 4;
+// console.log(num);
+
+// lion('Aslan');
+
+// let lion = function(name){
+//   console.log(`The lions name is ${lion}!`)
+// }
+
+/************ Tasks **************/
+
+// 1
+
+book("The Magician's Nephew");
+
+function book(title){
+  console.log(title)
+}
+
+
+// 2 
+
+// console.log(book);
+let books = 'the silver chair'
+console.log(books)
